b353f90c0b8e91d179332b8a08811711fb206a3c
Alexander Ovchinnikov <Alexander.Ovchinnikov@mendix.com>
2021-11-03 12:08:00 +0100

    Add assembly and package info

---
+++ Mendix.Web.Services.Discovery/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: AllowPartiallyTrustedCallers]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: CompilationRelaxations(CompilationRelaxations.NoStringInterning)]
